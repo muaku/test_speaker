67bfc454a31efadc9184588167aba1d92e207265
Muaku <muaku12@gmail.com>
2017-10-06 14:19:17 +0900

    testsdfdfedsdff

--- pyShutdown/pyShutdown.py
+++ pyShutdown/pyShutdown.py
@@ -4,29 +4,31 @@ import threading
 GPIO.setmode(GPIO.BCM)		# GPIO has 2 types of numbering system (this one is mainly used)
 GPIO.setup(5, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)		# set pin mode, set pin 5 as an INPUT (3th param is optional)
 GPIO.setup(6, GPIO.OUT)		# set pin 6 as an OUTPUT
-GPIO.setup(12, OUT)			# WDT (定期的にレベルを反転させる output pin)
+GPIO.setup(12, OUT)			# WDT 
 GPIO.setup(13, OUT)			# LED brink
 
 state = False				# initialize state as LOW
 
 def SHUT_REQ():
-	# シャットダウン処理開始
+	# Start shutdown process
 	GPIO.output(6, GPIO.LOW)	# GPIO.LOW or 0 or False (SUT_ACK)
 	# TODO: Run SHUT_DOWN func, if success then output pin 6 as HIGH
-	print("TODO: シャットダウン動作")
+	print("TODO: Shutdown process")
 	# SHUT_ACK
-	print("TODO: pin6 をHIGHにする")	
+	print("TODO: pin6 HIGH")	
 	GPIO.output(6, GPIO.HIGH)	
 
-# 動作状態の判定
+# Observe state
 def WDT():
-	# 4分間に1回 output信号を反転させる
+	#  ouput 4min per 1 time
 	threading.Timer(5, WDT).start()
-	GPIO.output(12, !state)
+	GPIO.output(12, state)
+	state = not state
+	print state
 
 WDT()
 
-# LED を光らせる
+# LED bright
 def MODE_LED_REQ():
 	GPIO.output(13, GPIO.LOW)	
 
