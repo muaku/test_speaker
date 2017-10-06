dabc7a88f6f0f0a83fa0d8e24a3a889db56bc095
Muaku <muaku12@gmail.com>
2017-10-06 12:42:56 +0900

    testsdfdfe

--- testSpeaker.py
+++ testSpeaker.py
@@ -9,10 +9,9 @@ from time import sleep
 if __name__=='__main__':
     commandText = u'/root/AquesTalkPi \"こんにちはケニーさん.今日の笑顔はいいですね.何か良いことありましたか.\" | sox -t wav -c1 - -t wav -c2 /dev/stdout | aplay -Dhw:2,0'
     subprocess.call(commandText, shell=True)
-    sleep(1)
+    sleep(2)
     commandText1 = u'/root/AquesTalkPi \"でも今朝は少しお熱があるみたい.お熱を計ってもらいましょう.\" | sox -t wav -c1 - -t wav -c2 /dev/stdout | aplay -Dhw:2,0'
     subprocess.call(commandText1, shell=True)
-    sleep(1)
-    commandText2 = u'/root/AquesTalkPi \"今日の心拍は64.呼吸は14です.体の調子は良いでしょう.\" | sox -t wav -c1 - -t wav -c2 /dev/stdout | aplay -Dhw:2,0'
+    sleep(2)
+    commandText2 = u'/root/AquesTalkPi \"今日の心拍は64.呼吸は14です.体の調子は良いですね.\" | sox -t wav -c1 - -t wav -c2 /dev/stdout | aplay -Dhw:2,0'
     subprocess.call(commandText2, shell=True)
-    sleep(1)
